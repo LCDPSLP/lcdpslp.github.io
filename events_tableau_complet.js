b8662307fa640b4a969742151c67b1a0292f9961
Les400coups <les400coups78@gmail.com>
2025-07-04 11:27:29 +0200

    fkrst

---
+++ events_tableau_complet.js
@@ -0,0 +1,71 @@
+
+const events_tableau = [
+
+/*
+  <span style='color:red'> pour la couleur
+  <br> saut de ligne
+ <a href='https://www.leboucanier.net' target='_blank'>www.leboucanier.net</a> pour lien direct
+*/
+
+
+{
+    id: "24-10-2025",
+    titre: "J'aime beaucoup ce que vous faites.",
+    date: "Le 24 Octobre 2025 à 20h",
+    date_iso: "2025-10-24",
+    lieu: "Les Automnales de Vernouillet",
+    description: "Comment une fausse manœuvre avec un téléphone portable vous fait découvrir ce que vos meilleurs amis pensent de vous en réalité, juste avant leur arrivée pour un week-end finalement pas comme les autres dans votre maison de campagne... <br> <br> Réservation :   <a href='mailto:lcdpslp78@gmail.com'>lcdpslp78@gmail.com</a>",
+    affiche: "images/Affiches/20251024.png",
+        highlight: false
+
+  },
+
+ {
+    id: "12-10-2025",
+    titre: "J'aime beaucoup ce que vous faites.",
+    date: "Le 12 Octobre 2025 à 16h30",
+    date_iso: "2025-10-12",
+    lieu: "Salle des Arts de Villennes sur Seine",
+    description: "Comment une fausse manœuvre avec un téléphone portable vous fait découvrir ce que vos meilleurs amis pensent de vous en réalité, juste avant leur arrivée pour un week-end finalement pas comme les autres dans votre maison de campagne... <br><br> Réservation :   <a href='mailto:lcdpslp78@gmail.com'>lcdpslp78@gmail.com</a>",
+    affiche: "images/Affiches/20251012.png",
+        highlight: false
+
+  },
+
+
+  {
+    id: "26-06-2025",
+    titre: "J'aime beaucoup ce que vous faites.",
+    date: "Le 26 Juin 2025 à 20h30",
+    date_iso: "2025-06-26",
+    lieu: "Espace Bosco - Vernouillet",
+    description: "Comment une fausse manœuvre avec un téléphone portable vous fait découvrir ce que vos meilleurs amis pensent de vous en réalité, juste avant leur arrivée pour un week-end finalement pas comme les autres dans votre maison de campagne... <br><br> Réservation :   <a href='mailto:lcdpslp78@gmail.com'>lcdpslp78@gmail.com</a>",
+    affiche: "images/Affiches/20250624.png",
+        highlight: false
+
+  },
+  
+  
+  
+   {
+    id: "22-06-2023",
+    titre: "Toc Toc",
+    date: "Le 22 Juin 2023 à 20h30",
+    date_iso: "2023-06-22",
+    lieu: "Espace Bosco - Vernouillet",
+    description: "Sept patients atteints de troubles obsessionnels compulsifs, Gilles de la Tourette, arithmomanie, nosophobie, tocs de vérification, fascination pour la symétrie, écholalie et paranoïa se retrouvent dans la salle d'attente du Dr Stern. <br>Êtes vous prêts pour cette thérapie complètement toquée ?!<br><br> Réservation :   <a href='mailto:lcdpslp78@gmail.com'>lcdpslp78@gmail.com</a>",
+    affiche: "images/Affiches/20230622.jpg",
+        highlight: false
+
+  }
+  
+  
+  
+  
+  
+  
+  
+];
+
+window.events_tableau_complet = events_tableau;
+window.events_tableau = events_tableau;
