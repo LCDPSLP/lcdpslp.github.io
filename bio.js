8c31dc8334ae438b46a9ca84e4947628ea68eba5
Les400coups <les400coups78@gmail.com>
2025-07-17 18:30:21 +0200

    chg

--- bio.js
+++ bio.js
@@ -1,7 +1,7 @@
 const bios = {
-  nina: "Chanteuse et professeure de Comédie musicale.",
-  eric: "Improvisateur chevronné et blagueur invétéré.",
-  fred: "Comédien assuré et passionné.",
-  lilou: "Improvisatrice de talent et professeure de théâtre.",
+  nina: "Comédienne & chanteuse, Nina fait vibrer les planches et les cordes vocales, avec la même intensité débordante.",
+  eric: "Comédien & improvisateur, Éric jongle avec les mots et les situations, avec la même spontanéité explosive.",
+  fred: "Comédien & bricoleur, Fred construit ses personnages comme il bricole, avec la même fantaisie appliquée.",
+  lilou: "Comédienne & prof de théâtre, Lilou manie les émotions sur scène comme en atelier, avec la même passion contagieuse.",
 
 };
